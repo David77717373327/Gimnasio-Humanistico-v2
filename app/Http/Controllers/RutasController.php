8721c5eaa63e9b0c2c1269b7f71155491ba5a14b
Jesus David <davidqzr.09@gmail.com>
2026-01-22 19:33:43 -0500

    Modificacion de Mayuscula

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class RutasController extends Controller
 {
+    /* ---------------------------------
+     |  CONTENIDO GENERAL
+     |----------------------------------*/
 
-        public function mostrarcontenidovideobienvenida()
-        {
-            return view('Video.video');
-        }
-
+    public function mostrarcontenidovideobienvenida()
+    {
+        return view('video.video');
+    }
 
     public function mostrarContenidoUsuario()
     {
@@ -28,108 +29,92 @@ class RutasController extends Controller
     {
         return view('student.index');
     }
+
     public function mostrarContenidoProfesor()
     {
         return view('professor.index');
     }
 
+    /* ---------------------------------
+     |  INSTITUCIÓN – QUIÉNES SOMOS
+     |----------------------------------*/
 
-
-
-    //Rutas para mostrar contenido de Quienes Somos
     public function mostrarcontenidohistoria()
     {
-        return view('Institucion.quienes_somos');
+        return view('institucion.quienes_somos');
     }
 
     public function mostrarcontenidosimbolosinstitucionales()
     {
-        return view('Institucion.Simbolos');
+        return view('institucion.simbolos');
     }
 
     public function mostrarcontenidomisionyvision()
     {
-        return view('Institucion.Mision_vision');
+        return view('institucion.mision_vision');
     }
 
-    public function mostrarcontenidoprincipiosyvarolres() 
+    public function mostrarcontenidoprincipiosyvarolres()
     {
-        return view('Institucion.Principios_y_valores');
+        return view('institucion.principios_y_valores');
     }
-
 
     public function mostrarcontenidofilosofiainstitucional()
     {
-        return view('Institucion.Filosofia_institucional');
+        return view('institucion.filosofia_institucional');
     }
+
+    /* ---------------------------------
+     |  ORGANIZACIÓN
+     |----------------------------------*/
 
     public function mostrarcontenidopoliticadecalidad()
     {
-        return view('Organización.Politica_de_calidad');
+        return view('organizacion.politica_de_calidad');
     }
 
-
-
-
-
-    //Rutas para mostrar contenido enfoque pedagodico
-    public function mostrarcontenidoenfoquepadagogico ()
-
+    public function mostrarcontenidoorganigrama()
     {
-        return view('Propuesta_Educativa.enfoque_pedagogico');
+        return view('organizacion.organigrama');
     }
 
-
-
-
-
-    //Rutas para mostras contenido de ORGANIZACION 
-
-        public function mostrarcontenidoorganigrama()
+    public function mostrarcontenidoGrupohumanistico()
     {
-        return view('Organización.organigrama');
-    }    
-
-
-     public function mostrarcontenidoGrupohumanistico()
-    {
-        return view('Organización.Grupo_humanistico');
-    } 
+        return view('organizacion.grupo_humanistico');
+    }
 
     public function mostrarcontenidoGobiernoescolar()
     {
-        return view('Organización.Gobierno_escolar');
+        return view('organizacion.gobierno_escolar');
     }
 
     public function mostrarcontenidoAdministrativos()
     {
-        return view('Organización.Administrativos');
+        return view('organizacion.administrativos');
     }
 
     public function mostrarcontenidoDocentes()
     {
-        return view('Organización.Docentes');
+        return view('organizacion.docentes');
     }
 
     public function mostrarcontenidoDirectivos()
     {
-        return view('Organización.Directivos');
+        return view('organizacion.directivos');
     }
 
+    /* ---------------------------------
+     |  PROPUESTA EDUCATIVA
+     |----------------------------------*/
 
-
-
-
-
-
-
-
-
-
-    public function mostrarcontenidoadmision()
+    public function mostrarcontenidoenfoquepadagogico()
     {
-        return view('admision.admision');
+        return view('propuesta_educativa.enfoque_pedagogico');
     }
+
+    /* ---------------------------------
+     |  NIVELES EDUCATIVOS
+     |----------------------------------*/
 
     public function mostrarcontenidoprecolar()
     {
@@ -140,31 +125,42 @@ class RutasController extends Controller
     {
         return view('niveles.basica_primaria');
     }
-    public function mostrarcontenidobasicasegundaria()
 
+    public function mostrarcontenidobasicasegundaria()
     {
         return view('niveles.basica_segundaria');
     }
 
-    public function mostrarcontenidomediaacademica ()
-
+    public function mostrarcontenidomediaacademica()
     {
         return view('niveles.media_academica');
     }
- 
 
-        public function mostrarcontenidotranporte ()
-        {
-            return view('Vida_Estudiantil.transporte');
-        }
+    /* ---------------------------------
+     |  VIDA ESTUDIANTIL
+     |----------------------------------*/
 
-        public function mostrarcontenidoproyectosinstitucionales ()
-        {
-            return view('Vida_Estudiantil.proyectos_institucionales');
-        }
+    public function mostrarcontenidotranporte()
+    {
+        return view('vida_estudiantil.transporte');
+    }
 
-        public function mostrarcontenidoeventoscelebraciones ()
-        {
-            return view('Vida_Estudiantil.Eventos_Celebraciones');
-        }
+    public function mostrarcontenidoproyectosinstitucionales()
+    {
+        return view('vida_estudiantil.proyectos_institucionales');
+    }
+
+    public function mostrarcontenidoeventoscelebraciones()
+    {
+        return view('vida_estudiantil.eventos_celebraciones');
+    }
+
+    /* ---------------------------------
+     |  ADMISIONES
+     |----------------------------------*/
+
+    public function mostrarcontenidoadmision()
+    {
+        return view('admision.admision');
+    }
 }
